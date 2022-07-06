ba70d3426398a05ee431b314da1d8b56e3b90826
Nick Goldman <niwago71@gmail.com>
2022-07-07 00:22:29 +0300

    styled a little bit and added some infos

--- bloger/settings.py
+++ bloger/settings.py
@@ -1,4 +1,5 @@
 
+
 """
 Django settings for bloger project.
 
@@ -10,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- bloger/urls.py
+++ bloger/urls.py
@@ -1,4 +1,5 @@
 
+
 """bloger URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
